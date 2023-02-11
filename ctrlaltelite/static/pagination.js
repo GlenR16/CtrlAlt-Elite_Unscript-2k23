4d462cadc8c3c3f39833faec2e36c709e7b6c286
Sachi017 <80409697+pysac17@users.noreply.github.com>
2023-02-11 22:21:36 +0530

    front end

---
+++ ctrlaltelite/static/pagination.js
@@ -0,0 +1,34 @@
+const table = document.querySelector(".responsive-table");
+const rows = table.querySelectorAll(".table-row");
+const pagination = document.querySelector("#pagination");
+const previous = document.querySelector("#previous");
+const next = document.querySelector("#next");
+
+let currentPage = 1;
+const rowsPerPage = 5;
+
+function showPage() {
+  const startRow = (currentPage - 1) * rowsPerPage;
+  const endRow = startRow + rowsPerPage;
+
+  for (let i = 0; i < rows.length; i++) {
+    const row = rows[i];
+    if (i >= startRow && i < endRow) {
+      row.style.display = "flex";
+    } else {
+      row.style.display = "none";
+    }
+  }
+}
+
+pagination.addEventListener("click", function(event) {
+  if (event.target === previous) {
+    currentPage--;
+  } else if (event.target === next) {
+    currentPage++;
+  }
+
+  showPage();
+});
+
+showPage();
