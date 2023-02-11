594ababf49b53b4e3ee70559cde8a03e08fd1a3a
Glen Rodrigues <glenrogers1234@gmail.com>
2023-02-11 11:45:40 +0530

    Commit 2

--- home/admin.py
+++ home/admin.py
@@ -4,9 +4,12 @@ from django.contrib import admin
 
 from django.contrib.admin.decorators import register
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import User,Hospital,Appointment,Invoice
 from .forms import UserChangeForm,UserCreationForm
 # Register your models here.
+admin.site.register(Hospital)
+admin.site.register(Appointment)
+admin.site.register(Invoice)
 
 @register(User)
 class UserAdmin(UserAdmin):
@@ -16,7 +19,7 @@ class UserAdmin(UserAdmin):
     list_display = ('email', 'name',)
     list_filter = ('is_active', 'is_superuser')
     fieldsets = (
-        ('User Data', {'fields': ('email', 'name', 'password')}),
+        ('User Data', {'fields': ('email', 'name', 'password','mobile','allergies','weight','hospital','invoices','appointments','dob')}),
         ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser', 'groups', 'user_permissions')}),  
     )
     add_fieldsets = (
--- home/models.py
+++ home/models.py
@@ -6,22 +6,37 @@ from django.contrib.auth.models import AbstractBaseUser,PermissionsMixin
 from .managers import UserManager
 
 # Create your models here.
+
 class Hospital(models.Model):
     name = models.CharField(max_length=127)
-
     def __str__(self):
         return self.name
-        
+
+class Invoice(models.Model):
+    date = models.DateField()
+    amount = models.IntegerField()
+    treatment = models.CharField(max_length=255)
+    def __str__(self):
+        return self.treatment
+
+class Appointment(models.Model):
+    hospital = models.ForeignKey(Hospital,on_delete=models.CASCADE)
+    doctor = models.CharField(max_length=255)
+    invoice = models.ForeignKey(Invoice,on_delete=models.CASCADE)
+    date = models.DateTimeField()
+    def __str__(self) -> str:
+        return self.doctor
+
 class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField("Email Address",unique=True,max_length=127)
     name = models.CharField(max_length=255)
-    mobile = models.CharField(max_length=15)
-    allergies = models.CharField(max_length=255)
-    weight = models.IntegerField()
-    hospital = models.ForeignKey(Hospital,on_delete=models.CASCADE)
-    invoices = models.ManyToManyField(Invoices)
-    appointments = models.ForeignKey()
-    dob = models.DateField()
+    mobile = models.CharField(max_length=15,default=0)
+    allergies = models.CharField(max_length=255,blank=True,null=True)
+    weight = models.IntegerField(default=0)
+    hospital = models.ForeignKey(Hospital,on_delete=models.CASCADE,blank=True,null=True)
+    invoices = models.ManyToManyField(Invoice)
+    appointments = models.ForeignKey(Appointment,on_delete=models.SET_NULL,null=True,blank=True)
+    dob = models.DateField(blank=True,null=True)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     date_joined = models.DateTimeField(auto_now=True)
@@ -32,16 +47,4 @@ class User(AbstractBaseUser, PermissionsMixin):
     def __str__(self):
         return self.name
 
-class Invoices(models.Model):
-    date = models.DateField()
-    amount = models.IntegerField()
-    treatment = models.CharField(max_length=255)
-    def __str__(self):
-        return self.treatment
 
-class appointments(models.Model):
-    hospital = models.ForeignKey(Hospital,on_delete=models.CASCADE)
-    doctor = models.CharField(max_length=255)
-    invoice = models.ForeignKey(Invoices,on_delete=models.CASCADE)
-    def __str__(self) -> str:
-        return self.date
