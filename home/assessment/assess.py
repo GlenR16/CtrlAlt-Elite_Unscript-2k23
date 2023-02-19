b1d8f7eadfaed64b987b6b95eb735a04690976a6
Glen Rodrigues <glenrogers1234@gmail.com>
2023-02-19 17:54:52 +0530

    Bug Fixes. Everything working now.

--- home/assessment/assess.py
+++ home/assessment/assess.py
@@ -7,14 +7,9 @@ model = load_model("./home/assessment/QualityAssess.h5")
 
 def assessment(file):
     # image = BytesIO(file)
-    img = image.load_img(file.file, target_size=(128, 128))
-    # Convert the image to a Numpy array
+    img = image.load_img(file, target_size=(128, 128))
     img_array = image.img_to_array(img)
-    # Add an extra dimension to the array to make it suitable for input to the model
     img_array = np.expand_dims(img_array, axis=0)
-    # Make a prediction on the image
     prediction = model.predict(img_array)
-    # Get the class with the highest predicted probability
     class_idx = np.argmax(prediction)
-    # Print the prediction
     return classed[class_idx],prediction[0, class_idx]
--- home/views.py
+++ home/views.py
@@ -4,19 +4,15 @@ from django.views.generic.base import TemplateView,RedirectView,View
 from .forms import UserCreationForm,UserLoginForm,PasswordChangeForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth import login,logout,authenticate,update_session_auth_hash
-from django.http import JsonResponse,HttpResponse
+from django.http import JsonResponse
 from .chatbot.chatbot import message as MessageChatBot
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 import json
-import os
-from zipfile import ZipFile
 from io import BytesIO
 from .assessment.assess import assessment
-from .models import UploadedFile
-import cv2
 import pytesseract
-import numpy
+from PIL import Image
 # Create your views here.
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 FaviconView = RedirectView.as_view(url='/static/favicon.ico', permanent=True)
@@ -27,39 +23,19 @@ class IndexView(TemplateView):
     def post(self,request):
         prediction = []
         confidence = []
+        textList = []
         files = request.FILES.getlist("file")
-        for i in UploadedFile.objects.filter(user=request.user.id):
-            try:
-                os.remove("./home/media/"+str(i.id))
-            except:
-                pass
-        
-        a = UploadedFile.objects.filter(user__id=request.user.id)
-        a.delete()
         for image in files:
+            image = BytesIO(image.read())
             p,c = assessment(image)
-            newfile = UploadedFile(file=image)
-            newfile.save()
-            with open("./home/media/"+str(newfile.id), 'wb') as f:
-                for chunk in image.chunks():
-                    f.write(chunk)
             if p == "Good":
-                img = cv2.imread("./home/media/"+str(newfile.id), cv2.COLOR_BGR2GRAY)
-                gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-                text = pytesseract.image_to_string(gray)
-                newfile.quality = p
-                newfile.ocr = text
-                newfile.save()
-            elif p == "Moderate":
-                text = None
+                text = pytesseract.image_to_string(Image.open(image))
+                textList.append(text)
             else:
-                text = None
-            newfile = UploadedFile(file=image,quality=p,ocr=text)
-            newfile.save()
-            request.user.files.add(newfile)
+                textList.append(None)
             prediction.append(p)
             confidence.append(c)            
-        out = zip(prediction,confidence,files,UploadedFile.objects.filter(user__email=request.user.email))
+        out = zip(prediction,confidence,files,textList)
         good = (prediction.count("Good")*100)/len(files)
         moderate = (prediction.count("Moderate")*100)/len(files)
         poor = (prediction.count("Poor")*100)/len(files)
@@ -85,7 +61,7 @@ class LoginView(TemplateView):
                 login(request,user)
                 return redirect("/dashboard")
             elif form2.is_valid():
-                user = form.save()
+                user = form2.save()
                 login(request,user)
                 return redirect("/dashboard")
             else:
